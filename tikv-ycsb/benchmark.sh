593f9f46cf469103a82cfc7147de4b0d7d6b60c5
Alex Chi <iskyzh@gmail.com>
2020-09-08 15:57:16 +0800

    faster running benchmarks

--- tikv-ycsb/benchmark.sh
+++ tikv-ycsb/benchmark.sh
@@ -22,24 +22,44 @@ function wait_tikv {
 
 for YCSB in 1kb 16kb
 do
-  for WORKLOAD in workloada workloadb workloadc workloadd workloade workloadf
+  for DB in default titan
   do
-    for DB in default titan
-    do
-      echo Running $WORKLOAD
-      rm -rf /home/tikv-data/*
-      tiup playground --kv.config config-${DB}.toml &
-      wait_tikv
-      echo "TiKV up"
-      sleep 10
-      (cd $GOPATH/src/github.com/pingcap/go-ycsb && ./bin/go-ycsb load tikv -P workloads/$WORKLOAD -P $DIR/ycsb_${YCSB}.conf | tee $DIR/logs/${DB}_${WORKLOAD}_${YCSB}.load.log)
-      sleep 5
-      (cd $GOPATH/src/github.com/pingcap/go-ycsb && ./bin/go-ycsb run tikv -P workloads/$WORKLOAD -P $DIR/ycsb_${YCSB}.conf  | tee $DIR/logs/${DB}_${WORKLOAD}_${YCSB}.run.log)
-      drop_cache_and_wait
-      pkill tiup
-      fg
-      sleep 30
-    done
+    rm -rf /home/tikv-data/*
+    tiup playground --kv.config config-${DB}.toml &
+    wait_tikv
+    echo "TiKV up"
+    sleep 10
+    export WORKLOAD=workloada
+    (cd $GOPATH/src/github.com/pingcap/go-ycsb && ./bin/go-ycsb load tikv -P workloads/$WORKLOAD -P $DIR/ycsb_${YCSB}.conf | tee $DIR/logs/${DB}_${WORKLOAD}_${YCSB}.load.log)
+    sleep 5
+    export WORKLOAD=workloada
+    (cd $GOPATH/src/github.com/pingcap/go-ycsb && ./bin/go-ycsb run tikv -P workloads/$WORKLOAD -P $DIR/ycsb_${YCSB}.conf  | tee $DIR/logs/${DB}_${WORKLOAD}_${YCSB}.run.log)
+    export WORKLOAD=workloadb
+    (cd $GOPATH/src/github.com/pingcap/go-ycsb && ./bin/go-ycsb run tikv -P workloads/$WORKLOAD -P $DIR/ycsb_${YCSB}.conf  | tee $DIR/logs/${DB}_${WORKLOAD}_${YCSB}.run.log)
+    export WORKLOAD=workloadc
+    (cd $GOPATH/src/github.com/pingcap/go-ycsb && ./bin/go-ycsb run tikv -P workloads/$WORKLOAD -P $DIR/ycsb_${YCSB}.conf  | tee $DIR/logs/${DB}_${WORKLOAD}_${YCSB}.run.log)
+    export WORKLOAD=workloadf
+    (cd $GOPATH/src/github.com/pingcap/go-ycsb && ./bin/go-ycsb run tikv -P workloads/$WORKLOAD -P $DIR/ycsb_${YCSB}.conf  | tee $DIR/logs/${DB}_${WORKLOAD}_${YCSB}.run.log)
+    export WORKLOAD=workloadd
+    (cd $GOPATH/src/github.com/pingcap/go-ycsb && ./bin/go-ycsb run tikv -P workloads/$WORKLOAD -P $DIR/ycsb_${YCSB}.conf  | tee $DIR/logs/${DB}_${WORKLOAD}_${YCSB}.run.log)
+    pkill tiup
+    fg
+    drop_cache_and_wait
+    sleep 30
+    
+    rm -rf /home/tikv-data/*
+    tiup playground --kv.config config-${DB}.toml &
+    wait_tikv
+    echo "TiKV up"
+    sleep 10
+    export WORKLOAD=workloade
+    (cd $GOPATH/src/github.com/pingcap/go-ycsb && ./bin/go-ycsb load tikv -P workloads/$WORKLOAD -P $DIR/ycsb_${YCSB}.conf | tee $DIR/logs/${DB}_${WORKLOAD}_${YCSB}.load.log)
+    sleep 5
+    export WORKLOAD=workloade
+    (cd $GOPATH/src/github.com/pingcap/go-ycsb && ./bin/go-ycsb run tikv -P workloads/$WORKLOAD -P $DIR/ycsb_${YCSB}.conf  | tee $DIR/logs/${DB}_${WORKLOAD}_${YCSB}.run.log)
+    pkill tiup
+    fg
+    drop_cache_and_wait
+    sleep 30
   done
 done
-
